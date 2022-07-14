78cedb04ad7ff0f758aa51127cc03f83783f32bd
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-07-14 17:44:44 -0500

    Update Exercise3.java

--- src/main/java/com/example/project/Exercise3.java
+++ src/main/java/com/example/project/Exercise3.java
@@ -20,7 +20,7 @@ public class Exercise3 {
 
 
     public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
-        ArrayList<String> lst= lista(a.postOrder());
+        ArrayList<String> lst= lista(a.preOrder());
         if(a.size % 2==0 || lst.size()== 1)
             return false;
         ArrayList<String> r= new ArrayList<String>();
