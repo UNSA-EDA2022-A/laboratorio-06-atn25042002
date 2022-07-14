a4492e2b8ac14fba23b08a8e01f61a63a981923b
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-07-14 14:35:37 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -19,8 +19,32 @@ public class Exercise1 {
         
     }
 
-
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
-        return a1.preOrder().equals(a2.preOrder());
+        ArrayList<String> lst1= lista(a1.postOrder());
+        ArrayList<String> lst2= lista(a2.postOrder());
+        HashMap<String, String> hash= new HashMap<String, String>();
+        if (lst1.size() != lst2.size())
+            return false;
+        else{
+            for (int i= 0; i< lst1.size(); i++) {
+                hash.put(lst1.get(i), lst2.get(i));
+            }
+        }
+        ArrayList<String> lstpre1= lista(a2.preOrder());
+        ArrayList<String> lstpre2= lista(a2.preOrder());
+        for (int i= 0; i< lstpre1.size(); i++) {
+            if(!lstpre2.get(i).equals(hash.get(lstpre1.get(i))))
+                return false;
+        }
+        return true;
+    }
+
+    public static ArrayList<String> lista(String p){
+        ArrayList<String> lst= new ArrayList<String>();
+        while(!p.equals(" ")){
+            lst.add(p.substring(0, p.indexOf(" ")));
+            p= p.substring(p.indexOf(" ")+ 1);
+        }
+        return lst;
     }
 }
