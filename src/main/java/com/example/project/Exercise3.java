69743e84ab382b9d8f9e6a0851612c925dabc753
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-07-14 19:18:35 -0500

    Update Exercise3.java

--- src/main/java/com/example/project/Exercise3.java
+++ src/main/java/com/example/project/Exercise3.java
@@ -20,30 +20,30 @@ public class Exercise3 {
 
     public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
         ArrayList<String> lst= lista(a.preOrder());
-        if(lst.size() % 2==0)
+        if(lst.size() % 2==0) //Para que cumpla debe tener un numero impar de elementos
             return false;
         return revisar(lst);
     }
 
     public static boolean revisar( ArrayList<String> lst){
-        if(lst.size()==1)
+        if(lst.size()==1) //Si es un numero si cumple
             return true;
         ArrayList<String> lst1= new ArrayList<String>();
         ArrayList<String> lst2= new ArrayList<String>();
         String str;
-        for (int i= 1; i< lst.size(); i++) {
+        for (int i= 1; i< lst.size(); i++) { //Separa en dos sub arboles
             str= lst.get(i);
             if(str.compareTo(lst.get(0))< 0)
                 lst1.add(str);
             else
                 lst2.add(str);
         }
-        if(lst2.isEmpty())
+        if(lst2.isEmpty()) // Si no hay un sub arbol derecho entonces no cumple
             return false;  
-        return revisar(lst1) && revisar(lst2);
+        return revisar(lst1) && revisar(lst2); //Verifica los dos subarboles
     }
     
-    public static ArrayList<String> lista(String p){
+    public static ArrayList<String> lista(String p){ //Convierte un texto con separadores en un ArrayList
         ArrayList<String> lst= new ArrayList<String>();
         while(p.indexOf(" ")>= 0){
             lst.add(p.substring(0, p.indexOf(" ")));
